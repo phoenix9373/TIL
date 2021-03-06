1de65940757607674cd0884a3fe6f5efaffeb3ae
유진우 <phoenix9373@naver.com>
2021-03-06 12:15:46 +0900

    210306 | javascript event delegation

--- JavaScript/DoYourSelf/shopping-sol/main.js
+++ JavaScript/DoYourSelf/shopping-sol/main.js
@@ -2,6 +2,7 @@ const items = document.querySelector(".items");
 const input = document.querySelector(".footer__input");
 const addBtn = document.querySelector(".footer__button");
 const itemCount = document.querySelector(".item__count");
+const removeAllBtn = document.querySelector(".items__remove");
 
 let count = 0;
 
@@ -23,46 +24,68 @@ function onAdd() {
   itemCount.innerHTML = count;
 }
 
+// id는 UUID 등을 활용하는 것이 좋다.
+let id = 0;
 // 아이템 생성
 function createItem(text) {
   const itemRow = document.createElement("li");
   itemRow.setAttribute("class", "item__row");
+  itemRow.setAttribute("data-id", id);
 
-  const item = document.createElement("div");
-  item.setAttribute("class", "item");
+  itemRow.innerHTML = `
+  <div class="item">
+    <span class="item__name">${text}</span>
+    <button class="item__delete">
+      <i class="fas fa-trash-alt" data-id=${id}></i>
+    </button>
+  </div>
+  <div class="item__divider"></div>
+  `;
 
-  const name = document.createElement("span");
-  name.setAttribute("class", "item__name");
-  name.innerHTML = text;
+  // 아이디를 증가시킨다.
+  id++;
 
-  const deleteBtn = document.createElement("button");
-  deleteBtn.setAttribute("class", "item__delete");
-  deleteBtn.innerHTML = '<i class="fas fa-trash-alt"></i>';
+  // const item = document.createElement("div");
+  // item.setAttribute("class", "item");
+
+  // const name = document.createElement("span");
+  // name.setAttribute("class", "item__name");
+  // name.innerHTML = text;
+
+  // const deleteBtn = document.createElement("button");
+  // deleteBtn.setAttribute("class", "item__delete");
+  // deleteBtn.innerHTML = `<i id="del_${item_id}" class="fas fa-trash-alt"></i>`;
   // Q. 어떻게 itemRow가 방금 만들어진 itemRow임을 알고 삭제할 수 있을까?
   // A. 클로저와 관련이 있다. itemRow를 전달할 때, 위에서 선언한 데이터를
   // "캡쳐"하듯이 전달한다. 따라서 어떤 itemRow인지 명확히 판별 가능하다.
-  deleteBtn.addEventListener("click", () => {
-    items.removeChild(itemRow);
-    count--;
-    itemCount.innerHTML = count;
-  });
+  // deleteBtn.addEventListener("click", () => {
+  //   items.removeChild(itemRow);
+  //   count--;
+  //   itemCount.innerHTML = count;
+  // });
 
-  const date = new Date();
-  const createdDate = document.createElement("span");
-  createdDate.setAttribute("class", "item__date");
-  createdDate.innerHTML = `${date.getFullYear()}. ${date.getMonth()}. ${date.getDate()}.`;
+  // const date = new Date();
+  // const createdDate = document.createElement("span");
+  // createdDate.setAttribute("class", "item__date");
+  // createdDate.innerHTML = `${date.getFullYear()}. ${date.getMonth()}. ${date.getDate()}.`;
 
-  const itemDivider = document.createElement("div");
-  itemDivider.setAttribute("class", "item__divider");
+  // const itemDivider = document.createElement("div");
+  // itemDivider.setAttribute("class", "item__divider");
 
-  item.appendChild(name);
-  item.appendChild(deleteBtn);
+  // item.appendChild(name);
+  // item.appendChild(deleteBtn);
 
-  itemRow.appendChild(item);
-  itemRow.appendChild(createdDate);
-  itemRow.appendChild(itemDivider);
+  // itemRow.appendChild(item);
+  // itemRow.appendChild(createdDate);
+  // itemRow.appendChild(itemDivider);
 
   return itemRow;
+}
+
+function removeAll() {
+  while (items.lastChild) {
+    items.removeChild(items.lastChild);
+  }
 }
 
 addBtn.addEventListener("click", () => {
@@ -73,4 +96,16 @@ input.addEventListener("keypress", (e) => {
   if (e.key === "Enter") {
     onAdd();
   }
+});
+
+items.addEventListener("click", (e) => {
+  const id = e.target.dataset.id;
+  if (id) {
+    const toBeDeleted = document.querySelector(`.item__row[data-id="${id}"]`);
+    toBeDeleted.remove();
+  }
+});
+
+removeAllBtn.addEventListener("click", () => {
+  removeAll();
 });
